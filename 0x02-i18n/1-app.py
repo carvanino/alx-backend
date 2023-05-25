7395fdb4970ba3fa9b2e52cc34057be46a62490e
Akinola <akinolatofunmi04@gmail.com>
2023-05-25 18:26:39 +0000

    Fix typo in shebang

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python3
-"""Module: Flask and Babel App"""
+"""
+Babel-Flask extension and setup
+"""
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
-class Config:
-    """App configurations"""
+app = Flask(__name__)
+
+
+class Config(object):
+    """
+    Configues available languages in our app
+    """
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app = Flask(__name__)
 app.config.from_object(Config)
-babel = Babel(app=app)
+babel = Babel(app)
 
 
 @app.route('/')
-def index():
-    """Render index page"""
-    return render_template("1-index.html")
+def index_1():
+    """
+    Renders templates/1-index.html
+    """
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5007)
