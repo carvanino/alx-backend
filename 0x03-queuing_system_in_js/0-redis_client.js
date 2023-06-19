f964df714b190a661fb1a26693f6aad2898dc276
Akinola <akinolatofunmi04@gmail.com>
2023-06-19 10:34:35 +0000

    Fix linters

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -7,5 +7,5 @@ client.on('connect', () => {
 });
 
 client.on('error', (error) => {
-  console.log(`Redis client not connected to the server: ${error}`)
+  console.log(`Redis client not connected to the server: ${error}`);
 });
