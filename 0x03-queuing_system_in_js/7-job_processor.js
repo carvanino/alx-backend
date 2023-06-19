6a03f00b2846fb4b0ddeffaf787ad631286c6351
Akinola <akinolatofunmi04@gmail.com>
2023-06-19 18:36:53 +0000

    Task 9 - Track progress and errors with Kue: Create the Job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,19 @@
+import kue from 'kue';
+
+const blackListedPhoneNumber = ['4153518780', '4153518781'];
+const queue = kue.createQueue();
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+  if (blackListedPhoneNumber.includes(phoneNumber)) {
+    return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  }
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+  return done();
+}
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
